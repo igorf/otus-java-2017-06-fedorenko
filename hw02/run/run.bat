d050a55bb079399544034b3f80eb14dbc80a6437
igor <pointer@nm.ru>
2017-06-13 00:33:31 +0400

    - Run scripts added

---
+++ hw02/run/run.bat
@@ -0,0 +1,2 @@
+cd ..
+java -javaagent:agent\target\memory-agent.jar -jar task\target\homework-task-02.jar
---
+++ hw02/run/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd ..
+java -javaagent:agent/target/memory-agent.jar -jar task/target/homework-task-02.jar
