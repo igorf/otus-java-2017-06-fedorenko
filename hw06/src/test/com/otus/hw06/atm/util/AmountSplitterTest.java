c6ffcd5ad3a1888ee65a0824a5124f8aa42cfb1e
igor <ifedorenko@gmail.com>
2017-07-18 00:23:14 +0400

    initial commit

---
+++ hw06/src/test/com/otus/hw06/atm/util/AmountSplitterTest.java
@@ -0,0 +1,19 @@
+package com.otus.hw06.atm.util;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AmountSplitterTest {
+    @Test
+    public void split() throws Exception {
+        List<Integer> denominations = new ArrayList<>();
+        denominations.add(15);
+        denominations.add(7);
+        denominations.add(5);
+        denominations.add(1);
+        List result = new AmountSplitter(denominations).split(16);
+        assert result.size() == 6;
+    }
+}
