f0cd195d79232a2d1fb8ac5d0b9b8ea34fe66002
igor <pointer@nm.ru>
2017-09-24 01:44:51 +0400

    добавил кеш менеджер в виде бина

--- hw15/web/src/main/java/com/otus/hw15/web/listeners/WebContextListener.java
+++ hw15/web/src/main/java/com/otus/hw15/web/listeners/WebContextListener.java
@@ -1,14 +1,14 @@
 package com.otus.hw15.web.listeners;
 
+import net.sf.ehcache.CacheManager;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 public class WebContextListener implements ServletContextListener {
     @Override
-    public void contextInitialized(ServletContextEvent arg0) {
-    }
+    public void contextInitialized(ServletContextEvent arg0) { }
 
     @Override
-    public void contextDestroyed(ServletContextEvent arg0) {
-    }
+    public void contextDestroyed(ServletContextEvent arg0) { }
 }
