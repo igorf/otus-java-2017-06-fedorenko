7ddd1b5543e48e00bca6a988b8fd7019ce9fc1b7
igor <ifedorenko@ibs.ru>
2017-09-26 18:15:05 +0400

    - Теперь нотификации приходят при имеющемся пользователе в кеше

--- hw15/db/src/main/java/com/otus/hw15/db/service/UserService.java
+++ hw15/db/src/main/java/com/otus/hw15/db/service/UserService.java
@@ -5,8 +5,11 @@ import com.otus.hw15.db.repository.UserRepository;
 import com.otus.hw15.msg.common.Address;
 import com.otus.hw15.msg.common.MessageBroker;
 import com.otus.hw15.msg.specific.messages.CacheChangedMessage;
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.Element;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,10 +18,27 @@ public class UserService {
     @Autowired private UserRepository userRepository;
     @Autowired private MessageBroker messageBroker;
     @Autowired private Address screenUpdaterAddress;
+    @Autowired private EhCacheCacheManager cacheManager;
 
-    @Cacheable(value="users", key="#id")
+    private final static String CACHE_KEY = "users"; //in real project use @Cacheable instead
+
     public User find(long id) {
-        User user = userRepository.findOne(id);
+        User user = null;
+        Cache cache = cacheManager.getCacheManager().getCache(CACHE_KEY);
+        boolean found = false;
+        if (cache != null) {
+            Element e = cache.get(id);
+            if (e != null) {
+                user = (User) e.getObjectValue();
+                found = true;
+            }
+        }
+        if (!found) {
+            user = userRepository.findOne(id);
+            if (cache != null && user != null) {
+                cache.put(new Element(id, user));
+            }
+        }
         messageBroker.sendMessage(screenUpdaterAddress, new CacheChangedMessage());
         return user;
     }
