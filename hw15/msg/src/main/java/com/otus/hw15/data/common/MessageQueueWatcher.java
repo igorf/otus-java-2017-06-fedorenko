5a772a6b954a65678106d44cc28f5356d8a05153
igor <ifedorenko@ibs.ru>
2017-09-27 14:15:00 +0400

    - Добавил нотификацию о слишком долгом выполнении запросов в очереди

--- hw15/msg/src/main/java/com/otus/hw15/data/common/MessageQueueWatcher.java
+++ hw15/msg/src/main/java/com/otus/hw15/data/common/MessageQueueWatcher.java
@@ -1,20 +1,22 @@
 package com.otus.hw15.data.common;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.logging.Logger;
 
 public class MessageQueueWatcher extends Thread {
     private ConcurrentLinkedQueue<AbstractMessage> messageQueue;
     private MessageAgent receiver;
-    private final static long SLEEP_INTERVAL = 10;
+    private final static long SLEEP_INTERVAL = 50;
+    private final static Logger logger = Logger.getLogger(MessageQueueWatcher.class.getName());
 
     MessageQueueWatcher(ConcurrentLinkedQueue<AbstractMessage> queue, MessageAgent receiver) {
         messageQueue = queue;
         this.receiver = receiver;
     }
 
-    private void rest() {
+    private void rest(long time) {
         try {
-            Thread.sleep(SLEEP_INTERVAL);
+            Thread.sleep(time);
         } catch (InterruptedException ex) {
             //Nothing to do
         }
@@ -26,16 +28,23 @@ public class MessageQueueWatcher extends Thread {
             try {
                 msg.run(receiver);
             } catch (Exception ex) {
-                //TODO: LOG!
-                ex.printStackTrace();
+                logger.severe(ex.getMessage());
             }
         }
     }
 
     public void run() {
         while(true) {
+            long begin = System.currentTimeMillis();
             tick();
-            rest();
+            long end = System.currentTimeMillis();
+            long elapsed = end - begin;
+
+            if (elapsed < SLEEP_INTERVAL) {
+                rest(SLEEP_INTERVAL - elapsed);
+            } else {
+                logger.severe("[!] Queue overflow! Elapsed time: " + elapsed);
+            }
         }
     }
 }
