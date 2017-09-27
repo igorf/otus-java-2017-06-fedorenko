bcaef5037118fa8eb68e83fc8f9572c9a0992338
igor <ifedorenko@gmail.com>
2017-09-27 23:49:21 +0400

    - Убрал лишнее

--- hw15/web/src/main/java/com/otus/hw15/web/service/LoginService.java
+++ hw15/web/src/main/java/com/otus/hw15/web/service/LoginService.java
@@ -2,8 +2,6 @@ package com.otus.hw15.web.service;
 
 import com.otus.hw15.data.model.User;
 import com.otus.hw15.data.specific.UserLoginService;
-import com.otus.hw15.db.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
@@ -13,7 +11,6 @@ public class LoginService implements UserLoginService {
 
     private final static String LOGGED_USER_ATTRIBUTE = "LOGGED_USER";
     private HttpSession session;
-    @Autowired private UserService userService;
 
     public boolean isLogged() {
         return getUser() != null;
@@ -21,12 +18,6 @@ public class LoginService implements UserLoginService {
 
     public User getUser() {
         return (User) session.getAttribute(LOGGED_USER_ATTRIBUTE);
-    }
-
-    public boolean login(String username, String password) {
-        User user = userService.findByLoginAndPassword(username, password);
-        setUserLogged(user);
-        return user != null;
     }
 
     @Override
